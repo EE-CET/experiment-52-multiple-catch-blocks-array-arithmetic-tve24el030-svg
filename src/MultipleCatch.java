5543542a44a0462de46d920d573c2e87f3fa2fa3
tve24el030-svg <tve24el030@cet.ac.in>
2026-02-20 09:30:59 +0530

    code uploaded

--- src/MultipleCatch.java
+++ src/MultipleCatch.java
@@ -10,5 +10,25 @@ public class MultipleCatch {
         // TODO: 
         // TODO: catch ArrayIndexOutOfBoundsException -> Print "Invalid Index"
         // TODO: catch ArithmeticException -> Print "Divide by zero error"
-    
+
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int[] arr=new int[N];
+		for(int i=0;i<N;i++){
+			arr[i]=sc.nextInt();
+		}
+		int ind=sc.nextInt();
+		int div=sc.nextInt();
+		try{
+			int res=arr[ind]/div;
+			System.out.print(res);
+		}
+		catch(ArrayIndexOutOfBoundsException e){
+			System.out.print("Invalid Index");
+		}
+		catch(ArithmeticException e){
+			System.out.print("Divide by zero error");
+		}
+	}
 }
